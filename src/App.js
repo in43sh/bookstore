dc657759d2effe6a845e564473fb88e4d8166c62
Sergey Sherstobitov <in43sh@gmail.com>
2018-12-21 15:20:11 -0800

    created content component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import NavBar from '../src/NavBar/NavBar';
+import Content from '../src/Content/Content';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Content />
       </div>
     );
   }
---
+++ src/Content/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="content__inner-container">
+          <h1>Books</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Content;
