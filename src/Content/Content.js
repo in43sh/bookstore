3f433c9524d2babd3597f364149153377037ee9f
Sergey Sherstobitov <in43sh@gmail.com>
2018-12-21 16:18:42 -0800

    setting up component structure

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './Content.css';
 
 class Content extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      books_total: [],
+      books_A_M: [],
+      books_N_Z: [],
+      books_others: [],
+      list1: '',
+      list2: '',
+    };
+}
   render() {
     return (
       <div className="content">
         <div className="content__inner-container">
-          <h1>Books</h1>
+          <h1 className="content__header">Books</h1>
+          <div className="books-list">
+            <h2 className="books-list__header">Titles A-M</h2>
+            <div className="books-list__item"></div>
+          </div>
         </div>
       </div>
     );
