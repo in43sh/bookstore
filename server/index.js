7df739b25cc03914006b7d259196d24721707fc8
Sergey Sherstobitov <in43sh@gmail.com>
2018-12-28 00:47:27 -0800

    added path support

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
 app.use(bodyParser.json());
